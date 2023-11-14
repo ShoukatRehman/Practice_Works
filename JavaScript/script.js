4177435a66d41586cfadb17a79879e242d05d847
ShoukatRehman <shoukatrahman2001@gmail.com>
2023-11-14 09:37:26 +0500

    Add some more loop

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,24 +1,23 @@
+var cubes = 'ABCDEFG';
+//styling console output using CSS with a %c format specifier
+for (var i = 0; i < 7; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
+var i = 3;
+var j = 5;
 
+if(i == 3 && j < 5) {
+  console.log("Hello");
+} else {
+  console.log("Goodbye");
+}
 
-//single loop
-for (var secondNum = 0; secondNum < 10; secondNum++) {
-    console.log(secondNum);
-}
-//nested loops - one inside another
-for (var firstNum = 0; firstNum < 2; firstNum++) {
-    for (var secondNum = 0; secondNum < 10; secondNum++) {
-        console.log(firstNum + ", " + secondNum);
-    }
-}
-//nested loops - one inside another
-for (var firstNum = 0; firstNum < 2; firstNum++) {
-    for (var secondNum = 0; secondNum < 10; secondNum++) {
-        console.log(firstNum + " times " + secondNum + " equals " + firstNum * secondNum);
-    }
-}
-//nested loops - one inside another 
-for (var i = 100; i > 10; i = i - 10) {
-    for (var j = 10; j > 4; j = j - 5) {
-        console.log(i + " divided by " + j + " equals " + i / j);
-    }
+var i = 7;
+var j = 2;
+
+if(i < 7 || j < 5) {
+  console.log("Hello");
+} else {
+  console.log("Goodbye");
 }
