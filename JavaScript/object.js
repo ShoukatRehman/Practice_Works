af5cb7a6fa893b73577d18e6f4d2967cfad66541
ShoukatRehman <shoukatrahman2001@gmail.com>
2023-11-15 17:39:12 +0500

    Objects

---
+++ JavaScript/object.js
@@ -0,0 +1,39 @@
+// //creating an object with properties and their values
+// var assistantManager = {
+//     rangeTilesPerTurn: 3,
+//     socialSkills: 30,
+//     streetSmarts: 30,
+//     health: 40,
+//     specialAbility: "young and ambitious",
+//     greeting: "Let's make some money"
+// }
+// var table = {
+//     legs: 3,
+//     color: "brown",
+//     priceUSD: 100,
+// }
+// console.log(table.priceUSD);//display the object in the developer console
+var fruits = [];
+fruits.push("apple"); // ['apple']
+fruits.push('pear'); // ['apple', 'pear']
+fruits.pop();
+console.log(fruits); // ['apple']
+
+function arrayBuilder(one, two, three) {
+    var arr = [];
+    arr.push(one);
+    arr.push(two);
+    arr.push(three);
+    console.log(arr);
+}
+arrayBuilder('apple', 'pear', 'plum'); // ['apple', 'pear', 'plum']
+
+function arrayBuilder(one, two, three) {
+    var arr = [];
+    arr.push(one);
+    arr.push(two);
+    arr.push(three);
+    return arr;
+}
+var simpleArr = arrayBuilder('apple', 'pear', 'plum');
+console.log(simpleArr); // ['apple','pear','plum']
