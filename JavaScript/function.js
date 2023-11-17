7acd20534c39e453286c00046b7b5e8c1eb9b75c
ShoukatRehman <shoukatrahman2001@gmail.com>
2023-11-17 08:50:47 +0500

    Additional

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -37,3 +37,28 @@ function listArrayItems(arr){
 }
 var colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
 listArrayItems(colors);
+
+var greeting = 'Howdy'; 
+
+console.log(greeting.length);
+
+console.log(greeting[0]);
+console.log(greeting[1]);
+
+for(var i= 0;i<greeting.length;i++){
+  console.log(greeting[i]);
+}
+//example of adding properties and methods to an object
+var car = {};
+car.mileage = 98765;
+car.color = "red";
+console.log(car);
+car.turnTheKey = function() {
+    console.log("The engine is running")
+}
+car.lightsOn = function() {
+    console.log("The lights are on.")
+}
+console.log(car);
+car.turnTheKey();
+car.lightsOn()
