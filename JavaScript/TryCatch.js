fcf8ed91491eda5c82cb2eee137ebf5fc0301551
ShoukatRehman <shoukatrahman2001@gmail.com>
2023-11-21 05:10:34 +0500

    Error

--- JavaScript/TryCatch.js
+++ JavaScript/TryCatch.js
@@ -1,3 +1,4 @@
+// Question no 01
 try{
     throw new ReferenceError();
 }
@@ -6,3 +7,47 @@ console.log(err)
 console.log('There was a reference error')
 }
 console.log('The program does not stop')
+//             Question no 02
+
+function addTwoNums(a,b){
+    try {
+if(typeof a != 'number'){
+    throw new ReferenceError('the first argument is not a number')
+}
+else if(typeof b != 'number'){
+
+    throw new ReferenceError('the second argument is not a number')
+}
+else{
+    console.log(a + b)
+}
+    }catch(err){
+        console.log("Error!",err);
+    }
+
+}
+addTwoNums(5,"5")
+console.log("It still works")
+
+//                   Question no 03
+function letterFinder (word,letter){
+    const condition1 = typeof(word) == 'string' && word.length >= 2;
+    const conditon2 = typeof(letter) == 'string' && letter.length === 1;
+
+    if(condition1 && conditon2){
+        for (let i = 0; i < word.length; i++) {
+      if (word[i] === letter) {
+        console.log(`Letter found at index ${i}`);
+        return true;
+      }
+        }console.log("Letter not found");
+    return false;
+  } 
+  else {
+        console.log("Please pass correct arguments to the function.");
+  }
+
+}
+letterFinder(5, 10);
+
+letterFinder("cat", "c");
